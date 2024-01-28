260f2fa75dee2230b601993e62433e98555035ea
Pik@chu <68701068+root-pik4chu@users.noreply.github.com>
2024-01-28 12:58:03 +0530

    added only data file...
    ..src .... project name- bg changer bt javascript and react ...

---
+++ newBgChangerData/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Dabba from './componant/Dabba';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+    <>
+    <App />
+    <Dabba />
+    </>
+
+);
+
+
