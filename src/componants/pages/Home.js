78956fcfb97d2f8bdd1a2570d017eec1a63d9a9e
pikachu <sahilsaundale@gmail.com>
2024-02-25 16:54:01 +0530

    edit Home

--- src/componants/pages/Home.js
+++ src/componants/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div className='bg-zinc-900'>
-      <h1 className="text-red-600 ">home</h1>
+      <h1 className="text-red-600 text-3xl">home</h1>
     </div>
   )
 }
